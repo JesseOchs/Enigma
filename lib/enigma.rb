44626c7f0b7f71639cb64718c58080a0068637fa
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-13 13:12:12 -0700

    Feat: add required methods to enigma class with notes

---
+++ lib/enigma.rb
@@ -0,0 +1,19 @@
+class Enigma
+
+  def initialize
+    
+  end
+
+  def encrypt(message, key, date)
+    # :encryption => the encrypted String
+    # :key => the key used for encryption as a String
+    # :date => the date used for encryption as a String in the form DDMMYY
+  end
+
+  def decrypt(ciphertext, key, date)
+    # :decryption => the decrypted String
+    # :key => the key used for decryption as a String
+    # :date => the date used for decryption as a String in the form DDMMYY
+  end
+
+end
