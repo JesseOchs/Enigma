d61eead1f77e9760af52519d19ae9612520c67bf
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-13 19:40:31 -0700

    Test: add test for character generator

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -3,12 +3,12 @@ require './spec_helper'
 
 describe Generator do
   before :each do
-
+    @enigma = Enigma.new
 
 
   end
 
-  xit 'can generate a random number' do 
+  xit 'can generate a random number' do
     expect(@gen_key).to be_a(Integer)
   end
 
@@ -16,4 +16,8 @@ describe Generator do
     expect(@gen_date).to be_a(Integer)
   end
 
+  xit 'can generate characters' do
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    expect(@enigma.gen_characters).to eq(expected)
+  end
 end
