6f6d1dec03cb7d43956c0128066fd27b152f3d0c
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-13 18:56:05 -0700

    Test: exists and attributes tests added for encrypt class

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -2,12 +2,34 @@ require './spec_helper'
 
 describe Encrypt do
   before :each do
-    @enigma = Encrypt.new
+    @encrypt_data = {
+                      decryption: "hello world",
+                      key: "02715",
+                      date: "040895"
+                    }
+    @decrypt_data = {
+                      encryption: "keder ohulw",
+                      key: "02715",
+                      date: "040895"
+                    }
+    @encrypt = Encrypt.new(@encrypt_data[:decryption], @encrypt_data[:key], @encrypt_data[:date])
 
 
   end
 
+  xit 'exists' do
+    expect(@encrypt).to be_a(Encrypt)
+  end
 
-
+  xit 'has attributes' do
+    expected = {
+      key: "02715",
+      date: "040895"
+    }
+    expect(@encrypt.message).to eq("hello world")
+    expect(@encrypt.key).to eq("02715")
+    expect(@encrypt.date).to eq("040895")
+    expect(@encrypt.encryption).to eq(expected)
+  end
 
 end
