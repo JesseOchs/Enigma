b253e586592beeb2846bef647d1242d0c02f73d4
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-15 10:59:31 -0700

    Test: test 1 passes, can create from input string

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -12,35 +12,16 @@ describe Encrypt do
                       key: "02715",
                       date: "040895"
                     }
-    @encrypt = Encrypt.new(@encrypt_data[:decryption], @encrypt_data[:key], @encrypt_data[:date])
+    @enigma = Enigma.new
 
 
   end
 
-  xit 'exists' do
-    expect(@encrypt).to be_a(Encrypt)
-  end
 
-  xit 'has attributes' do
-    expected = {
-      key: "02715",
-      date: "040895"
-    }
-    expect(@encrypt.message).to eq("hello world")
-    expect(@encrypt.key).to eq("02715")
-    expect(@encrypt.date).to eq("040895")
-    expect(@encrypt.encryption).to eq(expected)
-  end
+  it 'can create keys' do
+    expected = [12, 23, 34, 45]
+    expect(@enigma.make_keys("12345")).to eq(expected)
 
-  xit 'can create keys' do
-    expect(a key).to be_a(Integer)
-    expect(a key.size).to eq(2)
-    expect(b key).to be_a(Integer)
-    expect(b key.size).to eq(2)
-    expect(c key).to be_a(Integer)
-    expect(c key.size).to eq(2)
-    expect(d key).to be_a(Integer)
-    expect(d key.size).to eq(2)
   end
 
   xit 'can create offsets' do
