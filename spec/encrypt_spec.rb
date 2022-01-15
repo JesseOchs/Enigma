decffbed7b95170dbe72d9804cedbd3a9cda7581
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-15 11:08:00 -0700

    Test: test 3 passes, can generate shifts

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -21,14 +21,15 @@ describe Encrypt do
   it 'can create keys' do
     expected = [12, 23, 34, 45]
     expect(@enigma.make_keys("12345")).to eq(expected)
-
   end
 
   it 'can create offsets' do
     expected = [48, 88, 8, 4]
     expect(@enigma.make_offsets("150122")).to eq(expected)
-
-
   end
 
+  it 'can make shifts from keys and date' do
+    expected = [60, 111, 42, 49] 
+    expect(@enigma.make_shifts("12345", "150122")).to eq(expected)
+  end
 end
