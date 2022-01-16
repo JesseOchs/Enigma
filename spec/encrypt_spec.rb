22c869cef7e577587b7efaf11f8313b8b610eb5e
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-16 13:53:54 -0700

    Refactor: removed tests that do not pertain to this module

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -8,21 +8,6 @@ describe Encrypt do
   end
 
 
-  it 'can create keys' do
-    expected = [12, 23, 34, 45]
-    expect(@enigma.make_keys("12345")).to eq(expected)
-  end
-
-  it 'can create offsets' do
-    expected = [4, 8, 8, 4]
-    expect(@enigma.make_offsets("150122")).to eq(expected)
-  end
-
-  it 'can make shifts from keys and date' do
-    expected = [16, 31, 42, 49]
-    expect(@enigma.make_shifts("12345", "150122")).to eq(expected)
-  end
-
   it 'can swap characters' do
     expect(@enigma.character_swap("a")).to eq(0)
   end
