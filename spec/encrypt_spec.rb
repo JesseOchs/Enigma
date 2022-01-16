5dcc0cc6f2618ec28942b2f3f57f31e39d6afec8
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-16 09:41:24 -0700

    Test: all tests pass with 100% coverage for encrypt and shifts

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -22,4 +22,12 @@ describe Encrypt do
     expected = [16, 31, 42, 49]
     expect(@enigma.make_shifts("12345", "150122")).to eq(expected)
   end
+
+  it 'can swap characters' do
+    expect(@enigma.character_swap("a")).to eq(0)
+  end
+
+  it 'can encrypt a message' do
+    expect(@enigma.encrypt_message("hello world", "02715", "040895")).to eq("keder ohulw")
+  end
 end
--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -1,1 +1,21 @@
 require './spec_helper'
+
+
+describe Shifts do
+  before :each do
+
+    @enigma = Enigma.new
+
+  end
+
+  it 'can shift characters' do
+    expected = [["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c"],
+                ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "],
+                ["t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"],
+                ["u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"]]
+    expect(@enigma.shifted_characters("02715", "040895")).to eq(expected)
+  end
+
+
+
+end
