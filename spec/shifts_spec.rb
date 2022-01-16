946df83b52733e0cd3bfbb21d0d0e062a7fc15cc
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-16 13:50:32 -0700

    Test: all tests pass for shifts module

--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -8,6 +8,18 @@ describe Shifts do
 
   end
 
+  it 'can make keys' do
+    expect(@enigma.make_keys("02715")).to eq([2, 27, 71, 15])
+  end
+
+  it 'can make offsets' do
+    expect(@enigma.make_offsets("040895")).to eq([1, 0, 2, 5])
+  end
+
+  it 'can make shifts' do
+    expect(@enigma.make_shifts("02715", "040895")).to eq([3, 27, 73, 20])
+  end
+
   it 'can shift characters' do
     expected = [["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c"],
                 ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "],
