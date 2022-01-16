3f7ce6265501c2acf03351f2e89fab79165db9da
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-16 10:49:20 -0700

    Test: test 1 passes, can decrypt message

--- spec/decrypt_spec.rb
+++ spec/decrypt_spec.rb
@@ -3,35 +3,13 @@ require './spec_helper'
 
 describe Decrypt do
   before :each do
-    @encrypt_data = {
-                      decryption: "hello world",
-                      key: "02715",
-                      date: "040895"
-                    }
-    @decrypt_data = {
-                      encryption: "keder ohulw",
-                      key: "02715",
-                      date: "040895"
-                    }
-    @decrypt = Decrypt.new(@decrypt_data[:encryption], @decrypt_data[:key], @decrypt_data[:date])
-
-
+      @enigma = Enigma.new
   end
 
 
-  xit 'exists' do
-    expect(@decrypt).to be_a(Decrypt)
-  end
 
-  xit 'has attributes' do
-    expected = {
-      key: "02715",
-      date: "040895"
-    }
-    expect(@decrypt.ciphertext).to eq("keder ohulw")
-    expect(@decrypt.key).to eq("02715")
-    expect(@decrypt.date).to eq("040895")
-    expect(@decrypt.decryption).to eq(expected)
+  it 'can decrypt message' do
+    expect(@enigma.decrypt_message('keder ohulw', "02715", "040895")).to eq("hello world")
   end
 
 
