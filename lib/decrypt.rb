3d774d0f9f1f7dbddc51f3bc0d1f3850d81042dc
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-16 10:49:49 -0700

    Feat: add decrypt message method

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -6,8 +6,22 @@ module Decrypt
   include Shifts
 
 
-  def decrypt_message
-    @decryption
+  def decrypt_message(ciphertext, key, date)
+      rotated_characters = shifted_characters(key, date)
+      regular_chars = gen_characters
+    output = ""
+      ciphertext.each_char.with_index { |char, index|
+        if index % 4 == 0
+          output <<  regular_chars[rotated_characters[0].index(char)]
+        elsif index % 4 == 1
+          output << regular_chars[rotated_characters[1].index(char)]
+        elsif index % 4 == 2
+          output << regular_chars[rotated_characters[2].index(char)]
+        elsif index % 4 == 3
+          output << regular_chars[rotated_characters[3].index(char)]
+        end
+        }
+        return output
   end
 
 end
